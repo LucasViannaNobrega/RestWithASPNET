4164c1563220aa49f1fc0a14940f43daaeab635e
Lucas <lucashkd@gmail.com>
2021-05-26 17:06:19 -0300

    first commit

---
+++ RestWithASPNET/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestWithASPNET.Data.Converter.Contract;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Data.Converter.Implementations
+{
+    public class BookConverter : IParse<Book, BookVO>, IParse<BookVO, Book>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                Title = origin.Title,
+                Price = origin.Price,
+                LauchDate = origin.LauchDate
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                Title = origin.Title,
+                Price = origin.Price,
+                LauchDate = origin.LauchDate
+            };
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ RestWithASPNET/Data/VO/BookVO.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNET.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string Author { get; set; }
+        public DateTime LauchDate { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+    }
+}
